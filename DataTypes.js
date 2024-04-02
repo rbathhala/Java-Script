9b993ce53e6481ddf310d79884a51f2da65853b1
rbathhala <rbathhala@gmail.com>
2024-04-02 18:16:21 +0530

    DataTypes In JS

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+//String Data Type
+
+let empname="Rajesh"
+console.log(`value : ${empname} , type : ${typeof empname}`);
+
+//Number
+
+let EmpAge=33;
+console.log(`value : ${EmpAge}, type : ${typeof EmpAge}`);
+
+//Boolean
+
+let IsManager=true;
+console.log(`Value : ${IsManager}, Type : ${typeof IsManager}`);
+
+//Undefined
+
+let name;
+console.log(`Value : ${name}, Type : ${typeof name}`)
+
+//Null
+
+let Esal=null;
+console.log(`value : ${Esal}, Type : ${typeof Esal}`);
