60963758f33e240e327ef781f94d89dd840b1887
rbathhala <rbathhala@gmail.com>
2024-04-02 16:48:33 +0530

    Fist Java Script Program

---
+++ fisrt.js
@@ -0,0 +1,19 @@
+console.log("Java Script Basics")
+
+console.warn('This is for to shown Warning');
+
+console.error('This is for to shown warning');
+
+let student={
+    name:'Rajesh',
+    age:33,
+    course:'JS'
+
+}
+
+console.table(student)
+
+let a=10;
+let b=20;
+let sum=a+b;
+console.log(`the sum of ${a},${b} is : ${sum}`);
